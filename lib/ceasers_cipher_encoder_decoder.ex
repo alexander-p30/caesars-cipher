6367ddbf7f38ef1ef5bb78516e0700b2fc385190
alexander-p30 <alexanderp30e@gmail.com>
2021-03-27 19:11:43 -0300

    Initial commit

---
+++ lib/ceasers_cipher_encoder_decoder.ex
@@ -0,0 +1,18 @@
+defmodule CeasersCipherEncoderDecoder do
+  @moduledoc """
+  Documentation for `CeasersCipherEncoderDecoder`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CeasersCipherEncoderDecoder.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ceasers_cipher_encoder_decoder_test.exs
@@ -0,0 +1,8 @@
+defmodule CeasersCipherEncoderDecoderTest do
+  use ExUnit.Case
+  doctest CeasersCipherEncoderDecoder
+
+  test "greets the world" do
+    assert CeasersCipherEncoderDecoder.hello() == :world
+  end
+end
