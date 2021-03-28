2454df26fbcdd2368c86d5e200fe03d128de7a2b
alexander-p30 <alexanderp30e@gmail.com>
2021-03-27 22:21:46 -0300

    chore: fixes negative offset encoding

--- lib/ceasars_cipher.ex
+++ lib/ceasars_cipher.ex
@@ -19,9 +19,13 @@ defmodule CeasarsCipher do
   # Adding one is necessary to account for all alphabet letters
   defmacro max_cipher_offset, do: @last_upper_letter_code - @first_upper_letter_code + 1
 
-  def normalize_offset(cipher_offset) when cipher_offset <= 0,
-    do: normalize_offset(cipher_offset + max_cipher_offset())
+  def normalize_offset(char_number, cipher_offset)
+      when (char_number in @first_lower_letter_code..@last_lower_letter_code and
+              char_number + cipher_offset < @first_lower_letter_code) or
+             (char_number in @first_upper_letter_code..@last_upper_letter_code and
+                char_number + cipher_offset < @first_upper_letter_code),
+      do: normalize_offset(char_number, cipher_offset + max_cipher_offset())
 
-  def normalize_offset(cipher_offset),
+  def normalize_offset(_char_number, cipher_offset),
     do: cipher_offset |> rem(max_cipher_offset())
 end
--- lib/ceasars_cipher/encoder.ex
+++ lib/ceasars_cipher/encoder.ex
@@ -2,15 +2,13 @@ defmodule CeasarsCipher.Encoder do
   import CeasarsCipher
 
   def start(message, cipher_offset) do
-    cipher_offset = cipher_offset |> normalize_offset()
     message |> String.to_charlist() |> Enum.map(&encode_character(&1, cipher_offset)) |> to_string
   end
 
   defp encode_character(char_number, cipher_offset)
        when char_number in first_upper_letter_code()..last_upper_letter_code() or
               char_number in first_lower_letter_code()..last_lower_letter_code() do
-    IO.puts("#{char_number} + #{cipher_offset} = #{char_number + cipher_offset}")
-    char_number + cipher_offset
+    char_number + normalize_offset(char_number, cipher_offset)
   end
 
   defp encode_character(char_number, _cipher_offset) do
