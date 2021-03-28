6ac4180211c91b08492a07247bc25312a2865c37
alexander-p30 <alexanderp30e@gmail.com>
2021-03-27 22:04:24 -0300

    feat: implements ceasars encoding

--- lib/ceasars_cipher.ex
+++ lib/ceasars_cipher.ex
@@ -1,18 +1,27 @@
-defmodule CeasarsCipherEncoderDecoder do
+defmodule CeasarsCipher do
   @moduledoc """
-  Documentation for `CeasarsCipherEncoderDecoder`.
+  Documentation for `CeasarsCipher`.
   """
 
-  @doc """
-  Hello world.
+  @first_upper_letter_code 65
+  @last_upper_letter_code 90
+  @first_lower_letter_code 97
+  @last_lower_letter_code 122
 
-  ## Examples
+  defmacro first_upper_letter_code, do: @first_upper_letter_code
 
-      iex> CeasarsCipherEncoderDecoder.hello()
-      :world
+  defmacro last_upper_letter_code, do: @last_lower_letter_code
 
-  """
-  def hello do
-    :world
-  end
+  defmacro first_lower_letter_code, do: @first_lower_letter_code
+
+  defmacro last_lower_letter_code, do: @last_lower_letter_code
+
+  # Adding one is necessary to account for all alphabet letters
+  defmacro max_cipher_offset, do: @last_upper_letter_code - @first_upper_letter_code + 1
+
+  def normalize_offset(cipher_offset) when cipher_offset <= 0,
+    do: normalize_offset(cipher_offset + max_cipher_offset())
+
+  def normalize_offset(cipher_offset),
+    do: cipher_offset |> rem(max_cipher_offset())
 end
