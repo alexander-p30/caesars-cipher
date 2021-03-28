eb5081af68d17fd6918497c49e01e94d44c41bf5
alexander-p30 <alexanderp30e@gmail.com>
2021-03-28 10:16:01 -0300

    chore: reorganize modules and fix encoding

--- lib/ceasars_cipher.ex
+++ lib/ceasars_cipher.ex
@@ -3,29 +3,9 @@ defmodule CeasarsCipher do
   Documentation for `CeasarsCipher`.
   """
 
-  @first_upper_letter_code 65
-  @last_upper_letter_code 90
-  @first_lower_letter_code 97
-  @last_lower_letter_code 122
+  def encode(message, cipher_offset),
+    do: CeasarsCipher.Encoder.perform(message, cipher_offset)
 
-  defmacro first_upper_letter_code, do: @first_upper_letter_code
-
-  defmacro last_upper_letter_code, do: @last_lower_letter_code
-
-  defmacro first_lower_letter_code, do: @first_lower_letter_code
-
-  defmacro last_lower_letter_code, do: @last_lower_letter_code
-
-  # Adding one is necessary to account for all alphabet letters
-  defmacro max_cipher_offset, do: @last_upper_letter_code - @first_upper_letter_code + 1
-
-  def normalize_offset(char_number, cipher_offset)
-      when (char_number in @first_lower_letter_code..@last_lower_letter_code and
-              char_number + cipher_offset < @first_lower_letter_code) or
-             (char_number in @first_upper_letter_code..@last_upper_letter_code and
-                char_number + cipher_offset < @first_upper_letter_code),
-      do: normalize_offset(char_number, cipher_offset + max_cipher_offset())
-
-  def normalize_offset(_char_number, cipher_offset),
-    do: cipher_offset |> rem(max_cipher_offset())
+  def decode(message, cipher_offset),
+    do: CeasarsCipher.Encoder.perform(message, 0 - cipher_offset)
 end
--- lib/ceasars_cipher/encoder.ex
+++ lib/ceasars_cipher/encoder.ex
@@ -1,17 +1,40 @@
 defmodule CeasarsCipher.Encoder do
-  import CeasarsCipher
+  @first_upper 65
+  @last_upper 90
+  @first_lower 97
+  @last_lower 122
+  @max_offset 26
 
-  def start(message, cipher_offset) do
+  def perform(message, cipher_offset) do
     message |> String.to_charlist() |> Enum.map(&encode_character(&1, cipher_offset)) |> to_string
   end
 
   defp encode_character(char_number, cipher_offset)
-       when char_number in first_upper_letter_code()..last_upper_letter_code() or
-              char_number in first_lower_letter_code()..last_lower_letter_code() do
+       when char_number in @first_upper..@last_upper or
+              char_number in @first_lower..@last_lower do
     char_number + normalize_offset(char_number, cipher_offset)
   end
 
   defp encode_character(char_number, _cipher_offset) do
     char_number
   end
+
+  defp normalize_offset(char_number, cipher_offset)
+       when (char_number in @first_lower..@last_lower and
+               char_number + cipher_offset < @first_lower) or
+              (char_number in @first_upper..@last_upper and
+                 char_number + cipher_offset < @first_upper) do
+    normalize_offset(char_number, cipher_offset + @max_offset)
+  end
+
+  defp normalize_offset(char_number, cipher_offset)
+       when (char_number in @first_lower..@last_lower and
+               char_number + cipher_offset > @last_lower) or
+              (char_number in @first_upper..@last_upper and
+                 char_number + cipher_offset > @last_upper) do
+    normalize_offset(char_number, cipher_offset - @max_offset)
+  end
+
+  defp normalize_offset(_char_number, cipher_offset),
+    do: rem(cipher_offset, @max_offset)
 end
