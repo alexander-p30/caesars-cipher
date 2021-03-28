12a1c325332e5768319bf9a24f0a3c0809f156eb
alexander-p30 <alexanderp30e@gmail.com>
2021-03-27 22:05:06 -0300

    test: adds tests for ceasars cipher encoding

---
+++ test/ceasars_cipher_test.exs
@@ -0,0 +1,4 @@
+defmodule CeasarsCipherTest do
+  use ExUnit.Case
+  doctest CeasarsCipher
+end
--- test/ceasers_cipher_encoder_decoder_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule CeasarsCipherEncoderDecoderTest do
-  use ExUnit.Case
-  doctest CeasarsCipherEncoderDecoder
-
-  test "greets the world" do
-    assert CeasarsCipherEncoderDecoder.hello() == :world
-  end
-end
