7a6e1977d26c2de0a2c5ac84563e33354e448bb0
alexander-p30 <alexanderp30e@gmail.com>
2021-06-03 18:23:40 -0300

    update cipher repository state on CaesarsCipher encode/decode function calls

--- lib/caesars_cipher.ex
+++ lib/caesars_cipher.ex
@@ -3,9 +3,25 @@ defmodule CaesarsCipher do
   Documentation for `CaesarsCipher`.
   """
 
-  def encode(message, cipher_offset),
-    do: CaesarsCipher.Encoder.perform(message, cipher_offset)
+  def encode(message, cipher_offset) do
+    result = CaesarsCipher.Encoder.perform(message, cipher_offset)
 
-  def decode(message, cipher_offset),
-    do: CaesarsCipher.Encoder.perform(message, 0 - cipher_offset)
+    cast_message =
+      {:insert_encoded_cipher,
+       %{original: message, encoded: result, cipher_offset: cipher_offset}}
+
+    GenServer.cast(CaesarsCipher.Repository, cast_message)
+    result
+  end
+
+  def decode(message, cipher_offset) do
+    result = CaesarsCipher.Encoder.perform(message, 0 - cipher_offset)
+
+    cast_message =
+      {:insert_encoded_cipher,
+       %{original: result, encoded: message, cipher_offset: cipher_offset}}
+
+    GenServer.cast(CaesarsCipher.Repository, cast_message)
+    result
+  end
 end
